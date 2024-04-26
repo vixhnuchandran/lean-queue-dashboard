33710753d832ca6ea6f83d52726b4676caa9cf64
vixhnuchandran <vixhnuchandran@gmail.com>
2024-04-26 10:32:13 +0530

    Add route for the create-queue and add-tasks formpage

--- src/App.tsx
+++ src/App.tsx
@@ -13,35 +13,8 @@ import { Queues } from "./pages/Queues"
 import { Settings } from "./pages/Settings"
 import { Queue } from "./pages/Queue"
 import { Task } from "./pages/Tasks"
-import React, { Component, ErrorInfo } from "react"
-
-interface Props {
-  children: React.ReactNode
-}
-
-interface State {
-  hasError: boolean
-}
-
-class ErrorBoundary extends Component<Props, State> {
-  constructor(props: Props) {
-    super(props)
-    this.state = { hasError: false }
-  }
-
-  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
-    console.error("ErrorBoundary caught an error: ", error, errorInfo)
-    this.setState({ hasError: true })
-  }
-
-  render() {
-    if (this.state.hasError) {
-      return <h1>Something went wrong.</h1>
-    }
-
-    return this.props.children
-  }
-}
+import { AddQueue } from "./pages/AddQueue"
+import { AddTasks } from "./pages/AddTasks"
 
 function App() {
   const router = createBrowserRouter(
@@ -50,7 +23,9 @@ function App() {
         <Route index element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/queues" element={<Queues />} />
+        <Route path="/queues/create" element={<AddQueue />} />
         <Route path="/queues/:id" element={<Queue />} />
+        <Route path="/queues/:id/add" element={<AddTasks />} />
         <Route path="/queues/:id/:taskid" element={<Task />} />
         <Route path="/settings" element={<Settings />} />
       </Route>
@@ -58,12 +33,10 @@ function App() {
   )
 
   return (
-    <ErrorBoundary>
-      <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-        <RouterProvider router={router} />
-        <Toaster />
-      </ThemeProvider>
-    </ErrorBoundary>
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />
+      <Toaster />
+    </ThemeProvider>
   )
 }
 
