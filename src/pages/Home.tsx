16a67f9f0452db4f9f018cc86dc7f1c4544c7242
vixhnuchandran <vixhnuchandran@gmail.com>
2024-04-26 12:26:22 +0530

    Adjust logn-form size

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ import { SignupForm } from "../components/Signup"
 export function Home() {
   return (
     <>
-      <div className="flex justify-center items-center lg:h-[700px] mt-6 p-5">
+      <div className="flex justify-center items-center lg:h-[800px] mt-6 p-5">
         <Tabs defaultValue="login" className="w-[400px]">
           <TabsList className="grid w-full grid-cols-2">
             <TabsTrigger value="login">Login</TabsTrigger>
